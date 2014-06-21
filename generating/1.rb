048b128885f46e4194fc742c349a50d55e796380
Kromosbg <ivo383@abv.bg>
2014-06-21 20:27:03 +0300

    Update 1.rb

--- generating/1.rb
+++ generating/1.rb
@@ -1,27 +1,29 @@
-def rand_hexstr(length)
+def rand_generate(length)
   ((0..length).map{rand(256).chr}*"").unpack("H*")[0][0,length]
 end
- 
- 
-n1 = rand_hexstr(4)
-n2 = rand_hexstr(4)
+
+
+
+value = ""; 8.times{value  << (65 + rand(25)).chr}
+
+no = rand_generate(4)
+ni = rand_generate(4)
 
 File.open("testfile.c", "w") do |file| 
 	file.write("#include <stdio.h> \n 
 	int main(){ \n \t
-	int orig = 0x#{n1}; \n \t 
-	int num = 0x#{n2}; \n\t
+	int orig = 0x#{no}; \n \t 
+	int num = 0x#{ni}; \n\t
 	int result = orig & num; \n\t
 	printf(\"%d\",result); \n\t
 	return 0; \n}")
-
 end
 
-system("gcc test.c");
+system("gcc testfile.c");
 info = system("./a.out > info.txt");
 
-File.open('info.txt', 'r') do |f1|  
-  while line = f1.gets  
-    puts line  
-  end  
-end  
+f = File.open("info.txt", "r")
+f.each_line do |line|
+  puts line
+end
+f.close
