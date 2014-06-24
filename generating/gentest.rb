5e4689e34a4ab6183d587d708726af34e35df5f1
Kromosbg <ivo383@abv.bg>
2014-06-24 11:30:33 +0300

    Update gentest.rb

--- generating/gentest.rb
+++ generating/gentest.rb
@@ -1,13 +1,15 @@
 load 'num.rb'
 load 'general.rb'
 
+number_of_tests = ARGV[0]
+
 if number_of_tests == nil
 	number_of_tests = 1
 end
 
-#number_of_tests = ARGV[0]
  
-number_of_tests.to_f.times do |f|
+number_of_tests.to_f do |f|
 test = Test.new(f+1 )
 test.generate_tests
 end
+
